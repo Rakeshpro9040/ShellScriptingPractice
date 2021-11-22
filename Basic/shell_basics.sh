4500322eba68770a83a569bc3fa9a3863394f8ee
RakeshRoshanPanigrahi <r.roshan.panigrahi@accenture.com>
2021-11-22 13:24:10 +0000

    Nested Loops

--- Basic/shell_basics.sh
+++ Basic/shell_basics.sh
@@ -193,33 +193,52 @@ test_file='/home/mylocallinux/GitHub/ShellScriptingPractice/Basic/test.txt'
 ############# Decision Making #############
 
 ## if...else statement
-a=10
-b=20
+# a=10
+# b=20
 
-if [ $a == $b ]; then
-    echo "a is equal to b"
-elif [ $a -gt $b ]; then
-    echo "a is greater than b"
-elif [ $a -lt $b ]; then
-    echo "a is less than b"
-else
-    echo "None of the condition met"
-fi
+# if [ $a == $b ]; then
+#     echo "a is equal to b"
+# elif [ $a -gt $b ]; then
+#     echo "a is greater than b"
+# elif [ $a -lt $b ]; then
+#     echo "a is less than b"
+# else
+#     echo "None of the condition met"
+# fi
 
 ## case...esac statement
-FRUIT="kiwi"
+# FRUIT="kiwi"
 
-case "$FRUIT" in
-"apple")
-    echo "Apple pie is quite tasty."
-    ;;
-"banana")
-    echo "I like banana nut bread."
-    ;;
-"kiwi")
-    echo "New Zealand is famous for kiwi."
-    ;;
-esac
+# case "$FRUIT" in
+# "apple")
+#     echo "Apple pie is quite tasty."
+#     ;;
+# "banana")
+#     echo "I like banana nut bread."
+#     ;;
+# "kiwi")
+#     echo "New Zealand is famous for kiwi."
+#     ;;
+# esac
+
+# Test: ./shell_basics.sh
+# Output:
+
+###########################################
+
+############# Nested Loops #############
+
+## Nested While Loop
+a=0
+while [ "$a" -lt 10 ]; do # this is loop1
+    b="$a"
+    while [ "$b" -ge 0 ]; do # this is loop2
+        echo -n "$b "
+        b=$(expr $b - 1)
+    done
+    echo # This is for new line
+    a=$(expr $a + 1)
+done
 
 # Test: ./shell_basics.sh
 # Output:
